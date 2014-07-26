c25f6350682011901fc2ebd75bbc3d65d8237071
slick2 <slick2@HP-xw4400>
2014-07-26 17:48:26 +0800

    fixed the change password method

--- libraries/basic_auth.php
+++ libraries/basic_auth.php
@@ -16,7 +16,7 @@ class Basic_Auth {
 	public function __construct()
 	{
 		$this->load->config('basic_auth');
-		$this->load->model('basic_auth_model', 'Basic_Auth');
+		$this->load->model('basic_auth_model', 'Basic_auth_model');
 		$this->load->library('email');
 
 		$this->identity_column = $this->config->item('identity');
@@ -25,51 +25,27 @@ class Basic_Auth {
 		$this->email_templates = $this->config->item('email_templates');
 	}
 
+	/**
+	 * Method: __get
+	 * Getting the instance of CodeIgniter
+	 * 
+	 * @param mixed $var
+	 * @return mixed
+	 */
 	public function __get($var)
 	{
 		return get_instance()->$var;
 	}
 
-	public function activate($code)
-	{
-		//TODO : email activation
-	}
-
-	public function change_password($email, $new)//change the password by email
+	/**
+	 * Method: change_password
+	 * @param string $email
+	 * @param string $password
+	 * @return array
+	 */
+	public function change_password($email, $password)//change the password by email
 	{
 		//TODO : change password by identity username
-
-		return $this->Basic_Auth->change_password($identity, $new);
-	}
-
-	public function deactivate($identity)
-	{
-		//TODO: deactivate account
-	}
-
-	public function forgotten_password($email)
-	{
-		//TODO: forgotten password
-	}
-
-	public function forgotten_password_complete($code)
-	{
-		//TODO: forgotten password complete
-	}
-
-	public function is_logged()
-	{
-		$identity = $this->config->item('identity');
-		return ($this->session->userdata($identity)) ? TRUE : FALSE;
-	}
-
-	public function get_info($email)
-	{
-		return $this->Basic_Auth->get_info($email);
-	}
-
-	public function login($identity, $password)
-	{
 		switch ($this->config->item('basic_auth_mode'))
 		{
 			case 1 :
@@ -85,21 +61,65 @@ class Basic_Auth {
 				$password = $password;
 				break;
 		}
-		$profile = $this->Basic_Auth->login($identity, $password);
+		return $this->Basic_auth_model->change_password($email, $password);
+	}
+
+	/**
+	 * Method: is_logged
+	 * @return boolean
+	 */
+	public function is_logged()
+	{
+		$identity = $this->config->item('identity');
+		return ($this->session->userdata($identity)) ? TRUE : FALSE;
+	}
+
+	/**
+	 * Method: get_info
+	 * @param string $email
+	 * @return array
+	 */
+	public function get_info($email)
+	{
+		return $this->Basic_auth_model->get_info($email);
+	}
+
+	public function login($identity, $password)
+	{
+
+		switch ($this->config->item('basic_auth_mode'))
+		{
+			case 1 :
+				$password = $this->hash_password($password);
+				break;
+
+			case 2 :
+				$password = base64_encode($password);
+				break;
+
+			case 3 :
+			default :
+				$password = $password;
+				break;
+		}
+		$profile = $this->Basic_auth_model->login($identity, $password);
 
 		if (!empty($profile))
 		{
 			$this->session->set_userdata($this->identity_column, $identity);
-			return true;
+			return TRUE;
 		}
 		else
 		{
 			$this->errors[] = 'The ' . $this->identity_column . ' and password does not match';
 		}
 
-		return false;
+		return FALSE;
 	}
 
+	/**
+	 * Method:logout
+	 */
 	public function logout()
 	{
 		$identity = $this->config->item('identity');
@@ -107,16 +127,21 @@ class Basic_Auth {
 		$this->session->sess_destroy();
 	}
 
-	public function profile()
-	{
-		//TODO : profile
-	}
-
+	/**
+	 * Method:exist_email
+	 * @param string $email
+	 * @return boolean
+	 */
 	public function exist_email($email)
 	{
 		return $this->Basic_Auth->exist_email($email);
 	}
 
+	/**
+	 * Method: register
+	 * @param array $data
+	 * @return boolean
+	 */
 	public function register($data)
 	{
 		//check the data first
@@ -138,11 +163,11 @@ class Basic_Auth {
 				break;
 		}
 
-		$this->Basic_Auth->register($data);
+		$this->Basic_auth_model->register($data);
 
 		if ($this->config->item('email_activation'))
 		{
-			$this->Basic_Auth->deactivate($data[$this->identity_column]);
+			$this->Basic_auth_model->deactivate($data[$this->identity_column]);
 			$email_activation = array(
 				'email' => $data['email'],
 				'activation' => $this->Basic_Auth->activation_code
@@ -152,6 +177,11 @@ class Basic_Auth {
 		return TRUE;
 	}
 
+	/**
+	 * Method: hash_password
+	 * @param string $password
+	 * @return string
+	 */
 	public function hash_password($password = FALSE)
 	{
 		$salt_length = $this->config->item('salt_length');
@@ -160,23 +190,37 @@ class Basic_Auth {
 		{
 			return FALSE;
 		}
-		$salt = $this->salt();
+		$salt = $this->config->item('salt');
 
-		$password = $salt . substr(sha1($salt . $password), 0, -$salt_length);
+		$password = substr(sha1($salt . $password), 0, -$salt_length);
 
 		return $password;
 	}
 
+	/**
+	 * Method: salt
+	 * @return string
+	 */
 	public function salt()
 	{
 		return substr(md5(uniqid(rand(), true)), 0, $this->config->item('salt_length'));
 	}
 
+	/**
+	 * Method: errors
+	 * @return array
+	 */
 	public function errors()
 	{
 		return $this->errors;
 	}
 
+	/**
+	 * Method: email_activation
+	 * @param type $email
+	 * @param type $data
+	 * @return type
+	 */
 	public function email_activation($email, $data = array())
 	{
 

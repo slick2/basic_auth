370294834805d9cfbb59686e02cb9ce6a4305ca9
slick2 <carey.dayrit@gmail.com>
2014-08-09 21:52:06 +0800

    folder revisions

--- views/admin/auth/email/activation.php
+++
@@ -1,5 +0,0 @@
-Variables going to available to the activation email template.
-
-<?php print $password; ?>
-<?php print $email; ?>
-<?php print $activation; ?>
--- views/admin/auth/email/reset_password.php
+++
@@ -1,8 +0,0 @@
-Someone requested that the password be reset for the following account:
-
-If this was a mistake, just ignore this email and nothing will happen.
-
-To reset your password, visit the following address:
-
-<a href="<?php echo base_url() . 'auth/reset_confirm/' . $reset_code ?>"><?php echo base_url() . 'auth/reset_confirm/' . $reset_code ?></a>
-

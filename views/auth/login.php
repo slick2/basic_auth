dbd3a575ae5e3c28f4904e8102777e13fc568b12
slick2 <carey.dayrit@gmail.com>
2014-08-09 22:01:59 +0800

    template revisions

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,3 +1,5 @@
+<?php $this->load->view('common/header'); ?>
+
 <?php $this->load->view('auth/status'); ?>
 
 <h4>Login</h4>
@@ -21,3 +23,4 @@
 
 </form>
 
+<?php $this->load->view('common/footer'); ?>
